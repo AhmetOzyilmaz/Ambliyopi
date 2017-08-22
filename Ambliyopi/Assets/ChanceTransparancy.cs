c2be4fb9769a8e8e77fc5f09260810813c58b08b
AHMET ÖZYILMAZ <aozyilmaz35@gmail.com>
2017-08-22 10:43:20 +0300

    Added Some Scenes
    Added Transparancy Test Scenes
    Added 2 eye kamera and seeing differen color kube

---
+++ Ambliyopi/Assets/ChanceTransparancy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChanceTransparancy : MonoBehaviour {
+
+    public GameObject go;
+    public Scrollbar sb;
+    private float value = 0.0f;
+	// Use this for initialization
+	void Start () {
+        go.GetComponent<MeshRenderer>().material.color = new Color(go.GetComponent<MeshRenderer>().material.color.r, go.GetComponent<MeshRenderer>().material.color.g, go.GetComponent<MeshRenderer>().material.color.b, 0.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    public void ChanceTrans()
+    {
+        if(value<1.0)
+            go.GetComponent<MeshRenderer>().material.color = new Color(go.GetComponent<MeshRenderer>().material.color.r, go.GetComponent<MeshRenderer>().material.color.g, go.GetComponent<MeshRenderer>().material.color.b, 0.0f + sb.value);
+
+    }
+}
---
+++ Ambliyopi/Assets/Scripts/ChanceColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChanceColor : MonoBehaviour {
+
+    public Camera cam1;
+    public Camera cam2;
+
+	// Use this for initialization
+	void Start () {
+
+        cam1.enabled = true;
+        cam2.enabled = false;
+
+    }
+
+    void OnMouseDown()
+    {
+
+        if (cam1.enabled)
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+        }
+        else
+        {
+            cam1.enabled = true;
+            cam2.enabled = false;
+        }
+
+
+    }
+
+}
